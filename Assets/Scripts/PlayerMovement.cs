637261d40239db45803aa61322b64f5a705a4195
Sushixnoodle <145032867+Sushixnoodle@users.noreply.github.com>
2024-06-18 11:50:37 +0100

    omds sick movement from side to side like otmb of ye amask
    :green_heart:

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,73 +1,91 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-   enum Direction
-    {
-        North, South, East, West
-    }
+    public float speed = 5f;
+    private Rigidbody2D rb;
+    private bool isMoving = false;
 
-    public float speed;
-    Rigidbody2D rb;
-    Direction movingDir;
-
-     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0)
+        if (!isMoving)
         {
-            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            if (Input.GetAxisRaw("Horizontal") != 0)
+            {
+                rb.constraints = RigidbodyConstraints2D.FreezePositionY;
 
-            if (Input.GetAxisRaw("Horizontal")  > 0)
-            {
-                movingDir = Direction.East;
+                if (Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    MoveToWall(Vector2.right);
+                }
+                else
+                {
+                    MoveToWall(Vector2.left);
+                }
             }
-            else if (true)
+            else if (Input.GetAxisRaw("Vertical") != 0)
             {
-                movingDir = Direction.West;
-            } 
-        }
-        else if (Input.GetAxisRaw("Vertical") != 0)
-        {
-            rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+                rb.constraints = RigidbodyConstraints2D.FreezePositionX;
 
-            if (Input.GetAxisRaw("Vertical") > 0)
-            {
-                movingDir = Direction.North;
-            }
-            else if (true)
-            {
-                movingDir = Direction.South;
+                if (Input.GetAxisRaw("Vertical") > 0)
+                {
+                    MoveToWall(Vector2.up);
+                }
+                else
+                {
+                    MoveToWall(Vector2.down);
+                }
             }
         }
     }
 
+    void MoveToWall(Vector2 direction)
+    {
+        // Ignore the player's own collider
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, ~LayerMask.GetMask("Player"));
+        Debug.DrawRay(transform.position, direction * 100, Color.red, 2f); // Visualize the ray
+
+        if (hit.collider != null)
+        {
+            Debug.Log("Hit " + hit.collider.name);
+            StartCoroutine(MoveToPosition(hit.point));
+        }
+        else
+        {
+            Debug.Log("No hit detected");
+        }
+    }
+
+    IEnumerator MoveToPosition(Vector2 target)
+    {
+        isMoving = true;
+        Vector2 startPos = rb.position;
+        float elapsedTime = 0;
+        float journeyLength = Vector2.Distance(startPos, target);
+
+        while (Vector2.Distance(rb.position, target) > 0.01f)
+        {
+            elapsedTime += Time.deltaTime;
+            float fractionOfJourney = elapsedTime * speed / journeyLength;
+            rb.position = Vector2.Lerp(startPos, target, fractionOfJourney);
+            yield return null;
+        }
+
+        rb.position = target;
+        isMoving = false;
+        rb.constraints = RigidbodyConstraints2D.None; // Reset constraints
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation; // Only freeze rotation
+    }
+
     private void FixedUpdate()
     {
-        switch(movingDir)
-        {
-            case Direction.North:
-                rb.velocity = new Vector2(0, speed * Time.fixedDeltaTime);
-                break;
-            case Direction.South:
-                rb.velocity = new Vector2(0, -speed * Time.fixedDeltaTime);
-                break;
-            case Direction.East:
-                rb.velocity = new Vector2(speed * Time.fixedDeltaTime, 0);
-                break;
-            case Direction.West:
-                rb.velocity = new Vector2(-speed * Time.fixedDeltaTime, 0);
-                break;
-
-
-        }
+        // No need to set the velocity since we are directly setting the position
+        rb.velocity = Vector2.zero;
     }
 }
